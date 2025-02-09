22cbaa1fefee123f2040a39f88a0a0db431b0153
Mior Muhammad Zaki <crynobone@gmail.com>
2025-02-09 22:02:49 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Canvas.php
+++ src/Canvas.php
@@ -3,6 +3,7 @@
 namespace Orchestra\Canvas;
 
 use Illuminate\Support\Arr;
+use InvalidArgumentException;
 
 class Canvas
 {
@@ -16,13 +17,15 @@ class Canvas
         /** @var array<string, mixed> $configuration */
         $configuration = Arr::except($config, 'preset');
 
+        /** @var string|class-string<\Orchestra\Canvas\Presets\Preset> $preset */
         $preset = $config['preset'] ?? 'laravel';
 
+        /** @phpstan-ignore return.type */
         return match (true) {
             $preset === 'package' => new Presets\Package($configuration, $basePath),
             $preset === 'laravel' => new Presets\Laravel($configuration, $basePath),
             class_exists($preset) => new $preset($configuration, $basePath),
-            default => throw new InvalidArgumentException(sprintf('Unable to resolve %s preset', $preset)),
+            default => throw new InvalidArgumentException(\sprintf('Unable to resolve %s preset', $preset)),
         };
     }
 }
